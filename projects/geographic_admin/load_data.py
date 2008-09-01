39c0e72f067679e171c0262702a2222a2e764344
dane.springmeyer <dane.springmeyer@f561bbcf-a753-0410-87ae-9bb7ba73f0a0>
2008-09-01 21:49:38 +0000

    wrapped the add_postgis_srs function in a try to avoid exception if the projection already exists

--- projects/geographic_admin/load_data.py
+++ projects/geographic_admin/load_data.py
@@ -10,11 +10,16 @@
 
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+from psycopg2 import IntegrityError
 
 from django.contrib.gis.utils import mapping, LayerMapping, add_postgis_srs
 from world.models import WorldBorders
 
-add_postgis_srs(900913)
+try:
+    add_postgis_srs(900913)
+except IntegrityError:
+    print 'The Google Spherical Mercator projection, or a projection with srid 900913, already exists, skipping insert"
+
 world_borders = 'data/TM_WORLD_BORDERS_SIMPL-0.2/TM_WORLD_BORDERS_SIMPL-0.2.shp'
 layer = LayerMapping(WorldBorders,world_borders,mapping(world_borders, geom_name='geometry',multi_geom=True), encoding='Latin1')
 layer.save(verbose=True, progress=True)
