a231798c2167fd1eed89a29a09e185a410848db2
crschmidt <crschmidt@f561bbcf-a753-0410-87ae-9bb7ba73f0a0>
2008-09-18 01:22:41 +0000

    Removing World Borders from LayerMapping loader (since data isn't in SVN/setup), and adding comment explaining content

--- trunk/projects/cape/load_data.py
+++ trunk/projects/cape/load_data.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
+
+"""Utility for adding the Ward layermapping from data/ to the cape.lionhead
+   project."""
+
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from psycopg2 import IntegrityError
 
 from django.contrib.gis.utils import mapping, LayerMapping, add_postgis_srs
-from world.models import WorldBorders
 from lionshead.models import Ward
 
 try:
@@ -12,11 +15,8 @@ try:
 except IntegrityError:
     print "The Google Spherical Mercator projection, or a projection with srid 900913, already exists, skipping insert"
 
-world_borders = 'data/TM_WORLD_BORDERS_SIMPL-0.2/TM_WORLD_BORDERS_SIMPL-0.2.shp'
-layer = LayerMapping(WorldBorders,world_borders,mapping(world_borders, geom_name='geometry',multi_geom=True), encoding='Latin1')
-layer.save(verbose=True, progress=True)
-
 wards = 'data/wards_4326.shp'
-wlayer =  LayerMapping(Ward,wards,mapping(wards, geom_name='geometry',multi_geom=True), encoding='Latin1')
+wlayer =  LayerMapping(Ward, wards,
+    mapping(wards, geom_name='geometry',multi_geom=True), 
+    encoding='Latin1')
 wlayer.save(verbose=True, progress=True)
-
