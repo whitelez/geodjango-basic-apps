0cb92becb34ae8d1cc14a240bc154cd91aaaeb2f
dane.springmeyer <dane.springmeyer@f561bbcf-a753-0410-87ae-9bb7ba73f0a0>
2008-11-17 17:11:54 +0000

    Trivial code cleanup

--- projects/geographic_admin/world/models.py
+++ projects/geographic_admin/world/models.py
@@ -32,7 +32,7 @@ class WorldBorders(models.Model):
     # Regular Django fields corresponding to the attributes in the
     # world borders shapefile
     name = models.CharField(max_length=50)
-    area = models.IntegerField(help_text="Area of Country in SQ meters",)
+    area = models.IntegerField(help_text="Area of Country in SQ meters")
     pop2005 = models.IntegerField('Population 2005')
     fips = models.CharField('FIPS Code', max_length=2, help_text=mark_safe('<a href="http://www.census.gov/geo/www/fips/fips.html">Federal Information Processing Standard Code</a>'))
     iso2 = models.CharField('2 Digit ISO', max_length=2, help_text=mark_safe('<a href="http://www.iso.org/">International Organization for Standardization</a>' ))
@@ -40,12 +40,12 @@ class WorldBorders(models.Model):
     un = models.IntegerField('United Nations Code')
     region = models.IntegerField('Region Code')
     subregion = models.IntegerField('Sub-Region Code')
-    lon = models.FloatField('Longitude',help_text="Longitude of centerpoint", )
+    lon = models.FloatField('Longitude',help_text="Longitude of centerpoint")
     lat = models.FloatField('Latitude',help_text="Latitude of centerpoint")
     
     # GeoDjango-specific: a geometry field (MultiPolygonField), and
     # overriding the default manager with a GeoManager instance.
-    geometry = models.MultiPolygonField(srid=4326)
+    geometry = models.MultiPolygonField('Country Border',srid=4326)
     objects = models.GeoManager()
 
     # So the model is pluralized correctly in the admin.
